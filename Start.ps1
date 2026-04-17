1067f313284129cb8e3db0fbbf6539f8f678f0c4
Cédric Belin <cedx@outlook.com>
2026-04-17 22:23:46 +0200

    Update the dependencies [skip ci]

--- Start.ps1
+++ Start.ps1
@@ -1,7 +1,6 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
-Set-StrictMode -Version Latest
 
 $package = Get-Content "$PSScriptRoot/package.json" | ConvertFrom-Json
 node $package.bin.PhpMinifier @args
