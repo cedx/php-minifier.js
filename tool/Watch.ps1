e14bd0c0fbbeab1c5f837ace2a437ab3934cc0f4
Cédric Belin <cedx@outlook.com>
2026-05-07 22:23:58 +0200

    Update the build system [skip ci]

--- tool/Watch.ps1
+++ tool/Watch.ps1
@@ -1,4 +1,4 @@
 using module ./Cmdlets.psm1
 
 "Watching for file changes..."
-Invoke-TypeScript "$PSScriptRoot/../src/tsconfig.json" -SourceMap -Watch
+Invoke-TypeScript src/tsconfig.json -SourceMap -Watch
